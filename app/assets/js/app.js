3b59320b2007007a6e1d9259110dc87aae6bb52e
Christian Hall <jchristian192@gmail.com>
2014-02-20 02:23:00 -0600

    Added placeholders for screens that would take too long to implement by presentation
    Updated lots of other elements to prep for presentation.

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -10,102 +10,200 @@ $('.opening').show();
 // $('.transactions').show();
 // $('.transactions-cards').show();
 
+// Start
+$('.opening').addClass('bounce-in animated');
+
+function forward(current, next)
+{
+  $(current).addClass('fade-left-out animated');
+  setTimeout(function ()
+  {
+    $(current).hide();
+    $(current).removeClass('fade-right-in fade-left-out animated');
+    $(next).show();
+  }, 500);
+  $(next).addClass('fade-right-in animated');
+}
+
+function backward(current, next)
+{
+  $(current).addClass('fade-right-out animated');
+  setTimeout(function ()
+  {
+    $(current).hide();
+    $(current).removeClass('fade-left-in fade-right-out animated');
+    $(next).show();
+  }, 500);
+  $(next).addClass('fade-left-in animated');
+}
+
+function showModal(current, next)
+{
+  $(current).addClass('fade-left-out animated');
+  setTimeout(function ()
+  {
+    $(current).hide();
+    $(current).removeClass('fade-left-in fade-left-out animated');
+    $(next).show();
+  }, 500);
+  $(next).addClass('bounce-up-in animated');
+}
+
+function hideModal(current, next)
+{
+  $(current).addClass('bounce-up-out animated');
+  setTimeout(function ()
+  {
+    $(current).hide();
+    $(current).removeClass('bounce-up-in bounce-up-out animated');
+    $(next).show();
+  }, 500);
+  $(next).addClass('fade-left-in animated');
+}
+
+function paid()
+{
+  $('.overlay').show().addClass('fade-in animated');
+  $('.paid-modal').show().addClass('bounce-in animated');
+  setTimeout(function ()
+  {
+    $('.overlay').addClass('fade-out');
+    $('.paid-modal').addClass('bounce-out');
+  }, 1000);
+  setTimeout(function ()
+  {
+    $('.overlay').hide().removeClass('fade-in fade-out animated');
+    $('.paid-modal').hide().removeClass('bounce-in bounce-out animated');
+  }, 2000);
+}
+
 $('.opening .icon').click(function ()
 {
-  $('.opening').fadeOut('slow');
-  $('.home').delay(500).fadeIn('slow');
+  forward('.opening', '.home');
 });
 
 /* Forward */
 $('.home-cards .home-cell-top').click(function ()
 {
-  $('.home').fadeOut('slow');
-  $('.cards').delay(500).fadeIn('slow');
+  forward('.home', '.cards');
 });
 
 $('#cards-add-card').click(function ()
 {
-  $('.home').fadeOut('slow');
-  $('.add-card').delay(500).fadeIn('slow');
+  forward('.home', '.add-card');
 });
 $('#home-add').click(function ()
 {
-  $('.home').fadeOut('slow');
-  $('.add-card').delay(500).fadeIn('slow');
+  forward('.home', '.add-card');
 });
 $('#cards-add').click(function ()
 {
-  $('.cards').fadeOut('slow');
-  $('.add-card').delay(500).fadeIn('slow');
+  forward('.cards', '.add-card');
 });
 
 $('.cards .cell').click(function ()
 {
-  $('.cards').fadeOut('slow');
-  $('.edit-card').delay(500).fadeIn('slow');
+  forward('.cards', '.edit-card');
 });
 
-$('.home-pay').click(function ()
+$('.home-pay .home-cell-top').click(function ()
 {
-  $('.home').fadeOut('slow');
-  $('.pay').delay(500).fadeIn('slow');
+  forward('.home', '.pay');
+});
+
+$('#pay-pay-now').click(function ()
+{
+  showModal('.home', '.pay-now');
+});
+
+$('#pay-send-cash').click(function ()
+{
+  showModal('.home', '.send-cash');
 });
 
 $('.home-transactions').click(function ()
 {
-  $('.home').fadeOut('slow');
-  $('.transactions-cards').delay(500).fadeIn('slow');
+  forward('.home', '.transactions-cards');
 });
 
 $('.transactions-cards .cell-section').click(function ()
 {
-  $('.transactions-cards').fadeOut('slow');
-  $('.transactions').delay(500).fadeIn('slow');
+  forward('.transactions-cards', '.transactions');
 });
 
 $('.home-deals').click(function ()
 {
-  $('.home').fadeOut('slow');
-  $('.deals').delay(500).fadeIn('slow');
+  forward('.home', '.deals');
 });
+
+$('.deals-placeholder').click(function ()
+{
+  showModal('.deals', '.view-deals');
+});
+
+$('.settings-icon').click(function ()
+{
+  forward('.home', '.settings');
+});
+
 
 /* Back */
 $('.cards .back-icon').click(function ()
 {
-  $('.cards').fadeOut('slow');
-  $('.home').delay(500).fadeIn('slow');
+  backward('.cards', '.home');
 });
 $('.add-card .back-icon').click(function ()
 {
-  $('.add-card').fadeOut('slow');
-  $('.home').delay(500).fadeIn('slow');
+  backward('.add-card', '.home');
 });
 $('.edit-card .back-icon').click(function ()
 {
-  $('.edit-card').fadeOut('slow');
-  $('.cards').delay(500).fadeIn('slow');
+  backward('.edit-card', '.cards');
+});
+
+$('.pay-now-placeholder').click(function ()
+{
+  hideModal('.pay-now', '.home');
+  setTimeout(paid, 1000);
+});
+
+$('.send-cash-placeholder').click(function ()
+{
+  hideModal('.send-cash', '.home');
+  setTimeout(paid, 1000);
 });
 
 $('.pay .back-icon').click(function ()
 {
-  $('.pay').fadeOut('slow');
-  $('.home').delay(500).fadeIn('slow');
+  backward('.pay', '.home');
+});
+$('.pay-placeholder').click(function ()
+{
+  backward('.pay', '.home');
+  setTimeout(paid, 1000);
 });
 
 $('.transactions-cards .back-icon').click(function ()
 {
-  $('.transactions-cards').fadeOut('slow');
-  $('.home').delay(500).fadeIn('slow');
+  backward('.transactions-cards', '.home');
 });
 
 $('.transactions .back-icon').click(function ()
 {
-  $('.transactions').fadeOut('slow');
-  $('.home').delay(500).fadeIn('slow');
+  backward('.transactions', '.home');
 });
 
 $('.deals .back-icon').click(function ()
 {
-  $('.deals').fadeOut('slow');
-  $('.home').delay(500).fadeIn('slow');
+  backward('.deals', '.home');
+});
+
+$('.view-deals-placeholder').click(function ()
+{
+  hideModal('.view-deals', '.deals');
+});
+
+$('.settings-placeholder').click(function ()
+{
+  backward('.settings', '.home');
 });
