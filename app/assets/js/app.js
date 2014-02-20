3ea1dc2b55b987365cdd650bb2879ea911483c6f
Christian Hall <jchristian192@gmail.com>
2014-02-19 22:45:53 -0600

    Moar screens
    Cleaned up Add Cards.
    Added Edit Cards.
    Handled some basic transitions between screens.

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,5 +1,15 @@
 /* global $ */
 'use strict';
+
+// Show shortcuts
+$('.opening').show();
+// $('.home').show();
+// $('.cards').show();
+//$('.add-card').show();
+//$('.edit-card').show();
+// $('.transactions').show();
+// $('.transactions-cards').show();
+
 $('.opening .icon').click(function ()
 {
   $('.opening').fadeOut('slow');
@@ -7,10 +17,32 @@ $('.opening .icon').click(function ()
 });
 
 /* Forward */
-$('.home-cards').click(function ()
+$('.home-cards .home-cell-top').click(function ()
 {
   $('.home').fadeOut('slow');
   $('.cards').delay(500).fadeIn('slow');
+});
+
+$('#cards-add-card').click(function ()
+{
+  $('.home').fadeOut('slow');
+  $('.add-card').delay(500).fadeIn('slow');
+});
+$('#home-add').click(function ()
+{
+  $('.home').fadeOut('slow');
+  $('.add-card').delay(500).fadeIn('slow');
+});
+$('#cards-add').click(function ()
+{
+  $('.cards').fadeOut('slow');
+  $('.add-card').delay(500).fadeIn('slow');
+});
+
+$('.cards .cell').click(function ()
+{
+  $('.cards').fadeOut('slow');
+  $('.edit-card').delay(500).fadeIn('slow');
 });
 
 $('.home-pay').click(function ()
@@ -25,6 +57,12 @@ $('.home-transactions').click(function ()
   $('.transactions-cards').delay(500).fadeIn('slow');
 });
 
+$('.transactions-cards .cell-section').click(function ()
+{
+  $('.transactions-cards').fadeOut('slow');
+  $('.transactions').delay(500).fadeIn('slow');
+});
+
 $('.home-deals').click(function ()
 {
   $('.home').fadeOut('slow');
@@ -37,9 +75,37 @@ $('.cards .back-icon').click(function ()
   $('.cards').fadeOut('slow');
   $('.home').delay(500).fadeIn('slow');
 });
+$('.add-card .back-icon').click(function ()
+{
+  $('.add-card').fadeOut('slow');
+  $('.home').delay(500).fadeIn('slow');
+});
+$('.edit-card .back-icon').click(function ()
+{
+  $('.edit-card').fadeOut('slow');
+  $('.cards').delay(500).fadeIn('slow');
+});
+
+$('.pay .back-icon').click(function ()
+{
+  $('.pay').fadeOut('slow');
+  $('.home').delay(500).fadeIn('slow');
+});
 
 $('.transactions-cards .back-icon').click(function ()
 {
   $('.transactions-cards').fadeOut('slow');
+  $('.home').delay(500).fadeIn('slow');
+});
+
+$('.transactions .back-icon').click(function ()
+{
+  $('.transactions').fadeOut('slow');
+  $('.home').delay(500).fadeIn('slow');
+});
+
+$('.deals .back-icon').click(function ()
+{
+  $('.deals').fadeOut('slow');
   $('.home').delay(500).fadeIn('slow');
 });
